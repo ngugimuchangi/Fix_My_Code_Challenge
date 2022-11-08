498b79ab9308e0065d98fcd90ad72c5bf96776cd
ngugimuchangi <tech.curious14@gmail.com>
2022-11-08 04:46:49 +0000

    fix

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -8,10 +8,9 @@ class Square():
     """ Square class definition
         Class attributes: width, height
     """
-    width = 0
-    height = 0
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, width, **kwargs):
+        self.width = width
         """ Constructor method """
         for key, value in kwargs.items():
             setattr(self, key, value)
