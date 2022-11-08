3af7be17e7535204476b9c366cfa8518b049ec65
ngugimuchangi <tech.curious14@gmail.com>
2022-11-08 04:31:42 +0000

    square not rectangle

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -8,6 +8,8 @@ class square():
     """ Square class definition
         Class attributes: width, height
     """
+    width = 0
+    height = 0
 
     def __init__(self, *args, **kwargs):
         """ Constructor method """
@@ -16,20 +18,21 @@ class square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.height
+        return self.width * self.width
 
     def PermiterOfMySquare(self):
         """ Perimeter of the square"""
-        return (self.width * 2) + (self.height * 2)
+        return (self.width * 4)
 
     def __str__(self):
         """ String representation of the square """
-        return "{}/{}".format(self.width, self.height)
+        return "{}/{}".format(self.width, self.width)
 
 
 if __name__ == "__main__":
 
     s = square(width=12, height=9)
+    t = square(width=9, height=10)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
