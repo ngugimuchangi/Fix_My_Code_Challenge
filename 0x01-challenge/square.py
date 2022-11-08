d28a6c088bf26afd0a307713f323043c3cf8c439
ngugimuchangi <tech.curious14@gmail.com>
2022-11-08 04:55:01 +0000

    fix

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -11,24 +11,23 @@ class Square():
 
     width = 0
     height = 0
-    
+
     def __init__(self, *args, **kwargs):
         """ Constructor method """
         for key, value in kwargs.items():
-            if key == "width":
-                setattr(self, key, value)
+            setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def permiter_of_my_square(self):
         """ Perimeter of the square"""
-        return (self.width * 4)
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         """ String representation of the square """
-        return "{}/{}".format(self.width, self.width)
+        return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
