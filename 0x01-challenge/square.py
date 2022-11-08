441b88322a2b68b5934ccf457afea909202bc28e
ngugimuchangi <tech.curious14@gmail.com>
2022-11-08 04:42:43 +0000

    applied changes to running code

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -31,8 +31,8 @@ class Square():
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
-    t = square(width=9, height=10)
+    s = Square(width=12, height=9)
+    t = Square(width=9, height=10)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
